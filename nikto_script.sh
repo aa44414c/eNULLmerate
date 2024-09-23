52de355ede79a98f37660ae8d5e427fb2c603bb6
aa44414c <145689775+aa44414c@users.noreply.github.com>
2024-09-23 09:39:19 -0400

    Logic and format changes

--- nikto_script.sh
+++ nikto_script.sh
@@ -4,8 +4,11 @@ GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 WHITE='\033[0;1;37m'
 NC='\033[0m'
-echo -e "${ORANGE}-${NC} Executing ${WHITE}/usr/bin/nikto -h $1httprobe_output.txt -maxtime 15 >> $2nikto_output.txt${NC}..."
-if /usr/bin/nikto -h $1httprobe_output.txt -maxtime 5 >> $2nikto_output.txt | grep -q '+ ERROR: Host maximum execution time of 5 seconds reached'; then
-	echo -e "${RED}+${NC} ERROR: Host maximum execution time of 5 seconds reached..."
+
+echo -e "${ORANGE}-${NC} Executing ${WHITE}/usr/bin/nikto -h $1httprobe_output.txt -maxtime 5 >> $2nikto_output.txt${NC}..."
+/usr/bin/nikto -h $1httprobe_output.txt -maxtime 5 >> $2nikto_output.txt
+echo -e "${GREEN}+${NC} Done..."
+echo -e "${ORANGE}-${NC} Executing ${WHITE}grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' $1nikto_output.txt >> $2nikto_ipaddr_list.txt${NC}..."
+grep -oE "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" $1nikto_output.txt >> $2nikto_ipaddr_list.txt
 echo -e "${GREEN}+${NC} Done..."
 fi
