a19931b861671e2e6f3862a3ca67b10ba8909cad
aa44414c <145689775+aa44414c@users.noreply.github.com>
2024-09-23 09:51:52 -0400

    Updated logic and formatting

--- nmap_script.sh
+++ nmap_script.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+RED='\033[0;31m'
 GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 WHITE='\033[0;1;37m'
 NC='\033[0m'
-echo -e "${ORANGE}-${NC} Executing ${WHITE}/usr/bin/nmap -iL $1assetfinder_output.txt -T3 -oN $2nmap_output.txt${NC}..."
-/usr/bin/nmap -iL $1assetfinder_output.txt -T3 -oN $2nmap_output.txt
+
+echo -e "${ORANGE}-${NC} Executing ${WHITE}/usr/bin/nmap -iL $1amass_ipaddr_list.txt -T3 -oN $2nmap_amass_output.txt${NC}..."
+/usr/bin/nmap -iL $1amass_ipaddr_list.txt -T3 -oN $2nmap_amass_output.txt
+echo -e "${ORANGE}-${NC} Executing ${WHITE}/usr/bin/nmap -iL $1assetfinder_ipaddr_list.txt -T3 -oN $2nmap_assetfinder_output.txt${NC}..."
+/usr/bin/nmap -iL $1assetfinder_ipaddr_list.txt -T3 -oN $2nmap_assetfinder_output.txt
+echo -e "${ORANGE}-${NC} Executing ${WHITE}/usr/bin/nmap -iL $1nikto_ipaddr_list.txt -T3 -oN $2nmap_nikto_output.txt${NC}..."
+/usr/bin/nmap -iL $1nikto_ipaddr_list.txt -T3 -oN $2nmap_nikto_output.txt
 echo -e "${GREEN}+${NC} Done..."
+fi
