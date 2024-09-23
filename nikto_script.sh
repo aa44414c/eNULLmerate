a0e979a95b1904045c7eae0aa1def9a552e71a11
aa44414c <145689775+aa44414c@users.noreply.github.com>
2024-09-23 09:48:30 -0400

    Additional logic and formatting

--- nikto_script.sh
+++ nikto_script.sh
@@ -7,7 +7,6 @@ NC='\033[0m'
 
 echo -e "${ORANGE}-${NC} Executing ${WHITE}/usr/bin/nikto -h $1httprobe_output.txt -maxtime 5 >> $2nikto_output.txt${NC}..."
 /usr/bin/nikto -h $1httprobe_output.txt -maxtime 5 >> $2nikto_output.txt
-echo -e "${GREEN}+${NC} Done..."
 echo -e "${ORANGE}-${NC} Executing ${WHITE}grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' $1nikto_output.txt >> $2nikto_ipaddr_list.txt${NC}..."
 grep -oE "[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}" $1nikto_output.txt >> $2nikto_ipaddr_list.txt
 echo -e "${GREEN}+${NC} Done..."
