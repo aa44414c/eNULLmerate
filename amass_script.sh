10ff0651f3bf28c7ce74874254e6dea111738d9d
aa44414c <145689775+aa44414c@users.noreply.github.com>
2024-09-23 09:37:25 -0400

    Logic and format changes

--- amass_script.sh
+++ amass_script.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
+RED='\033[0;31m'
 GREEN='\033[0;32m'
 ORANGE='\033[0;33m'
 WHITE='\033[0;1;37m'
 NC='\033[0m'
-echo -e "${ORANGE}-${NC} Executing ${WHITE}/usr/bin/amass enum -d $1 >> $2amass_output.txt${NC}..."
-/usr/bin/amass enum -d $1 >> $2amass_output.txt
-echo -e "${GREEN}+${NC} Done..."
+
+if [ "$1" == "" ];then
+	echo "${RED}-${NC} You did not provide a domain."
+	echo "${RED}-${NC} Syntax:  /usr/share/scripts/amass_script.sh <domain> <output_path>"
+	echo "${RED}-${NC} Example: /usr/share/scripts/amass_script.sh google.com /usr/outputs/"
+elif [ "$2" == "" ];then
+	echo "${RED}-${NC} You did not provide an output path."
+	echo "${RED}-${NC} Syntax:  /usr/share/scripts/amass_script.sh <domain> <output_path>"
+	echo "${RED}-${NC} Example: /usr/share/scripts/amass_script.sh google.com /usr/outputs/"
+else
+	echo -e "${ORANGE}-${NC} Executing ${WHITE}amass enum -d $1 >> $2amass_output.txt${NC}..."
+	amass enum -d $1 >> $2amass_output.txt
+	echo -e "${GREEN}+${NC} Done..."
+fi
